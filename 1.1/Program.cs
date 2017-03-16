9289787de8d466b717db2a8b2cc90d890b4ef98a
alex.paclin <alex.paclin@gmail.com>
2017-03-16 17:28:07 +0500

    arrays & strings 1.1, 1.2

---
+++ 1.1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+1.1 Реализуйте алгоритм, определяющий, все ли символы в строке встречаются только один раз.
+	А если при этом запрещено использование дополнительных структур данных?
+	Используется набор символов ASCII.
+*/
+
+namespace _1._1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"IsUniqueChars: {IsUniqueChars("asdfga")}");
+			Console.WriteLine($"IsUniqueChars2: {IsUniqueChars2("asdfg")}");
+			Console.ReadKey();
+		}
+
+		// O(N)
+		private static bool IsUniqueChars(string str)
+		{
+			if (str.Length > 128)
+			{
+				return false;
+			}
+
+			bool[] charSet = new bool[128];
+			foreach (var c in str)
+			{
+				if (charSet[c]) // Символ уже встречался в строке
+				{
+					return false;
+				}
+				charSet[c] = true;
+			}
+
+			return true;
+		}
+
+		// O(N)
+		private static bool IsUniqueChars2(string str)
+		{
+			int checker = 0;
+
+			foreach (var c in str)
+			{
+				int val = c - 'a';
+				if ((checker & (1 << val)) > 0)
+				{
+					return false;
+				}
+				checker |= (1 << val);
+			}
+
+			return true;
+		}
+
+		/*
+		1. Сравнить каждый символ строки со всеми остальными символами строки. 
+			Это потребует О(N^2) времени и О(1) памяти.  
+		2. Если изменение строки разрешено, то можно ее отсортировать (что потребует
+		   О(N log(N)) времени), а затем последовательно проверить строку на идентичность
+		   соседних символов. Будьте внимательны: некоторые алгоритмы сортировки
+		   требуют больших объемов памяти.
+		*/
+	}
+}
---
+++ 1.2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+1.2 Для двух строк напишите метод, проверяющий является ли одна строка перестановкой другой?
+	Используется набор символов ASCII.
+*/
+
+namespace _1._2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"Permutation: {Permutation("asd", "dsa")}");
+			Console.WriteLine($"Permutation2: {Permutation2("asd", "dsa")}");
+			Console.ReadKey();
+		}
+
+		private static string Sort(string str)
+		{
+			var content = str.ToCharArray();
+			Array.Sort(content);
+			return new string(content);
+		}
+
+		private static bool Permutation(string str1, string str2)
+		{
+			if (str1.Length != str2.Length)
+				return false;
+			return Sort(str1).Equals(Sort(str2));
+		}
+
+		private static bool Permutation2(string str1, string str2)
+		{
+			if (str1.Length != str2.Length)
+				return false;
+
+			var letters = new int[128];
+
+			foreach (var c in str1)
+			{
+				letters[c]++;
+			}
+
+			foreach (var c in str2)
+			{
+				letters[c]--;
+				if (letters[c] < 0)
+					return false;
+			}
+
+			return true;
+		}
+	}
+}
